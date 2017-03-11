2dfe8255068af714663b4b84c3aa7a8fc0bc4906
KoopieCo <koopieco@gmail.com>
2017-03-11 00:15:53 -0600

    Initial Commit

---
+++ src/main/java/com/koopieco/AuraballApplication.java
@@ -0,0 +1,12 @@
+package com.koopieco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuraballApplication.class, args);
+	}
+}
---
+++ src/test/java/com/koopieco/AuraballApplicationTests.java
@@ -0,0 +1,16 @@
+package com.koopieco;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuraballApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
